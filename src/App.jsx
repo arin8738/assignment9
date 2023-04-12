78d28e5e38708a98b829b3663b04a9a8af4e27c0
arin8738 <arinsikder06@gmail.com>
2023-04-12 21:18:19 +0600

    assingment-9 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './componeds/nav/nav'
-import Banner from './componeds/banner/Banner'
-import Job from './componeds/job/Job'
 import { Outlet, useActionData, useLoaderData } from 'react-router-dom'
-import Outlets from './componeds/outlets/Outlets'
+
 
 function App() {
 
@@ -15,7 +13,6 @@ function App() {
     <div className="App">
      <Nav></Nav>
     <Outlet></Outlet>
-
     </div>
   )
 }
